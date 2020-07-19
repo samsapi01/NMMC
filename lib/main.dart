05afcbfb3d9ffeefc4d1f9071d3923c55ebe3b46
samsapiol <digis3c@protonmail.com>
2020-07-20 01:47:30 +0530

    loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(MyApp()); 
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,25 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(        
-        body: Container(   
+      home: Scaffold(
+        body: Container(
             decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.center,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.indigo[400],
-                Colors.indigo[900]                
-              ] 
-            )
-          ),     
-          child: LogInPage()
-        ),
-      ), 
+                gradient: LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.indigo[400], Colors.indigo[900]])),
+            child: LogInPage()),
+      ),
     );
   }
 }
-
 
 class LogInPage extends StatefulWidget {
   @override
@@ -35,95 +28,78 @@ class LogInPage extends StatefulWidget {
 }
 
 class _LogInPageState extends State<LogInPage> {
+  int _pagestate = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-      child: Column(
-        children: <Widget>[
-          Container(        
-            child: 
-            Image.asset('assets/images/bg.jpg')
-            ),
-          Container(            
-            padding: EdgeInsets.fromLTRB(110, 20, 110, 20),
-            child: 
-            Image.asset('assets/images/logo.PNG'),
-            ),
-          Container(
-            child: Container(
-              padding: EdgeInsets.only(bottom: 20),             
-              child: 
-              Text(
-                'Navi Mumbai Municipal Corporation',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white
-                ),
-                )
-              ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 20),
-            child: Column(
-            children:<Widget> [ 
+        child: Column(children: <Widget>[
+      Container(child: Image.asset('assets/images/bg.jpg')),
+      Container(
+        padding: EdgeInsets.fromLTRB(110, 20, 110, 20),
+        child: Image.asset('assets/images/logo.PNG'),
+      ),
+      Container(
+        child: Container(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text(
+              'Navi Mumbai Municipal Corporation',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            )),
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 20),
+        child: Column(
+          children: <Widget>[
             Text(
               'Welcome to',
               style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white                
-               ),
-              ),
-              SizedBox(height: 20),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 20),
             Text(
               'MY',
               style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
-               ),
-              ),
-              SizedBox(height: 10),
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 10),
             Text(
               'CLASSROOM',
               style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
-               ),
-              ),
-            ],
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
-            ),
-          Container(
-            margin: EdgeInsets.all(15),
-            padding: EdgeInsets.all(10),
-            child:GestureDetector(
-              onTap: () {  
-
-              },
-                child: Container(
-                padding: EdgeInsets.all(10),
-                width: MediaQuery.of(context).size.width,            
-                decoration: BoxDecoration(
+          ],
+        ),
+      ),
+      Container(
+          margin: EdgeInsets.all(15),
+          padding: EdgeInsets.all(10),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                _pagestate = 1;
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
                   color: Colors.indigo[400],
-                  borderRadius: BorderRadius.circular(50)
-                ),
-                child: Center(
-                  child: Text(
-                    'SIGN IN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22
-                    ),
-                    ),
+                  borderRadius: BorderRadius.circular(50)),
+              child: Center(
+                child: Text(
+                  'SIGN IN',
+                  style: TextStyle(color: Colors.white, fontSize: 22),
                 ),
               ),
-            )
             ),
-        ]
-      )
-    );
+          )),
+    ]));
   }
 }
